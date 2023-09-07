832acedceb2b10a5fe77215c6d465c66d900fb69
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-09-07 11:29:41 +0100

    add input text handling and buttons to PaceInput

--- src/components/PaceInput.tsx
+++ src/components/PaceInput.tsx
@@ -11,22 +11,31 @@ export default function PaceInput({ pace, changePace }: PaceComponentProps) {
   useEffect(() => {
     const minutes = Math.floor(pace / 60);
     const seconds = pace % 60;
-    setNewPace(`${minutes}:${String(seconds).padStart(2, "0")}`);
-  }, [pace]);
+    const paceString = `${minutes}:${String(seconds).padStart(2, "0")}`;
+    setNewPace(paceString);
+  }, []);
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const newValue = e.target.value;
-    setNewPace(newValue);
-    const newMinutes = parseInt(newValue.split(":")[0]);
-    const newSeconds = parseInt(newValue.split(":")[1]);
-    console.log(newMinutes, newSeconds);
-    changePace(newMinutes * 60 + newSeconds);
+    let newValue = e.target.value;
+    newValue = newValue.replace(/\D/g, "").padStart(4, "0");
+    const newMinutes = newValue.slice(-4, -2);
+    const newSeconds = newValue.slice(-2);
+    setNewPace(`${newMinutes}:${newSeconds}`);
+    changePace(Number(newMinutes) * 60 + Number(newSeconds));
   }
+
+  const handleClick = (increment: number) => () => {
+    changePace(pace + increment);
+  }
+
   return (
-    <label className="input-container">
-      Pace
-      <input value={newPace} onChange={handleChange} />
-      {/* <input value={currentPace} onChange={handleChange} type="number" /> */}
-    </label>
+    <>
+      <label className="input-container">
+        Pace
+        <input value={newPace} onChange={handleChange} inputMode="numeric" />
+      </label>
+      <button onClick={handleClick(1)}>Up</button>
+      <button onClick={handleClick(-1)}>Down</button>
+    </>
   );
 }
