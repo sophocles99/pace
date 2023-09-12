6d90508dd8a9922fe83af05a81566e0af6a4d50c
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-09-12 17:04:35 +0100

    start incorporating React-Select component

--- src/components/DistanceSelect.tsx
+++ src/components/DistanceSelect.tsx
@@ -1,8 +1,15 @@
 import { ChangeEvent, useEffect, useState } from "react";
+import Select, { Options } from "react-select";
+import round from "../utils/round";
 
 type DistanceProps = {
   distance: number;
   changeValue: (valueType: string, newDistance: number) => void;
+};
+
+type Option = {
+  value: number;
+  label: string;
 };
 
 const raceDistances: Record<string, number> = {
@@ -16,26 +23,36 @@ const raceDistances: Record<string, number> = {
   Marathon: 42.195,
 };
 
+const distanceOptions: Options<Option> = [
+  { value: 1.609344, label: "1 mile" },
+  { value: 5, label: "5K" },
+  { value: 10, label: "10K" },
+  { value: 15, label: "15K" },
+  { value: 16.09344, label: "10 miles" },
+  { value: 21.0975, label: "Half Marathon" },
+  { value: 32.18688, label: "20 miles" },
+  { value: 42.195, label: "Marathon" },
+];
+
 export default function DistanceSelect({
   distance,
   changeValue,
 }: DistanceProps) {
-  const [selectedDistance, setSelectedDistance] = useState("");
+  const [selectedDistance, setSelectedDistance] = useState<Option | null>(null);
 
   useEffect(() => {
-    const matchingDistanceName: string =
-      Object.keys(raceDistances).find(
-        (raceDistanceName) =>
-          Math.round(distance * 100) / 100 ===
-          Math.round(raceDistances[raceDistanceName] * 100) / 100
-      ) || "";
-    setSelectedDistance(matchingDistanceName);
+    const matchingDistanceOption: Option | null =
+      distanceOptions.find(
+        (distanceOption) =>
+          round(distance, 2) === round(distanceOption.value, 2)
+      ) || null;
+    setSelectedDistance(matchingDistanceOption);
   }, [distance]);
 
-  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    const newDistanceName = event.target.value;
-    setSelectedDistance(newDistanceName);
-    changeValue("distance", raceDistances[newDistanceName]);
+  const handleChange = (newOption: Option) => {
+    console.log(newOption);
+    setSelectedDistance(newOption);
+    changeValue("distance", newOption.value);
   };
 
   return (
@@ -45,12 +62,13 @@ export default function DistanceSelect({
       </div>
       <div className="input-container">
         <div className="input-block distance-select">
-          <select
+          <Select
             id="distanceSelect"
+            options={distanceOptions}
             onChange={handleChange}
             value={selectedDistance}
-          >
-            <option value="" disabled>
+          />
+          {/* <option value="" disabled>
               Choose a distance
             </option>
             {Object.keys(raceDistances).map((raceDistanceName) => {
@@ -60,7 +78,7 @@ export default function DistanceSelect({
                 </option>
               );
             })}
-          </select>
+          </select> */}
         </div>
       </div>
     </section>
