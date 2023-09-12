21041ffaa081bc64390819b5d36e91f417de53a8
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-09-12 21:22:53 +0100

    remove unused code

--- src/components/DistanceSelect.tsx
+++ src/components/DistanceSelect.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Select from "react-select";
 import round from "../utils/round";
 
@@ -10,17 +10,6 @@ type DistanceProps = {
 type Option = {
   value: number;
   label: string;
-};
-
-const raceDistances: Record<string, number> = {
-  "1 mile": 1.609344,
-  "5K": 5,
-  "10K": 10,
-  "15K": 15,
-  "10 miles": 16.09344,
-  "Half Marathon": 21.0975,
-  "20 miles": 32.18688,
-  Marathon: 42.195,
 };
 
 const distanceOptions: Option[] = [
@@ -70,17 +59,6 @@ export default function DistanceSelect({
             onChange={handleChange}
             value={selectedDistance}
           />
-          {/* <option value="" disabled>
-              Choose a distance
-            </option>
-            {Object.keys(raceDistances).map((raceDistanceName) => {
-              return (
-                <option value={raceDistanceName} key={raceDistanceName}>
-                  {raceDistanceName}
-                </option>
-              );
-            })}
-          </select> */}
         </div>
       </div>
     </section>
