01f8e48df02a158fa6baa4a65e81cf8b1e8d06d2
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-09-14 17:17:19 +0100

    further style React-Select component and remove input spinner on Firefox

--- src/styles/selectStyles.tsx
+++ src/styles/selectStyles.tsx
@@ -1,63 +1,66 @@
 import { StylesConfig } from "react-select";
 
 const selectStyles: StylesConfig = {
-  control: (defaultStyles, { menuIsOpen }) => {
-    return {
-      ...defaultStyles,
-      height: "4rem",
-      boxShadow: "none",
-      borderColor: menuIsOpen ? "var(--foreground)" : "var(--background)",
-      borderRadius: "0.5rem",
-      backgroundColor: "inherit",
-      color: "inherit",
-      caretColor: "transparent",
+  control: (defaultStyles, { menuIsOpen }) => ({
+    ...defaultStyles,
+    height: "4rem",
+    boxShadow: "none",
+    borderColor: menuIsOpen ? "var(--foreground)" : "var(--background)",
+    borderRadius: "0.5rem",
+    backgroundColor: "inherit",
+    color: "inherit",
+    caretColor: "transparent",
+    ":hover": {
+      ...defaultStyles[":hover"],
+      borderColor: menuIsOpen ? "var(--foreground)" : "var(--foreground-dim)",
+    },
+  }),
 
-      ":hover": {
-        ...defaultStyles[":hover"],
-        borderColor: menuIsOpen ? "var(--foreground)" : "var(--foreground-dim)",
-      },
-    };
-  },
   singleValue: (defaultStyles) => ({
     ...defaultStyles,
     color: "var(--foreground)",
   }),
+
   dropdownIndicator: (defaultStyles) => ({
     ...defaultStyles,
     padding: "0 8px 0 0",
   }),
+
   indicatorSeparator: (defaultStyles) => ({
     ...defaultStyles,
     display: "none",
   }),
+
   menu: (defaultStyles) => ({
     ...defaultStyles,
     borderRadius: "0.5rem",
-    marginTop: "0.25rem"
+    marginTop: "0.25rem",
   }),
+
   menuList: (defaultStyles) => ({
     ...defaultStyles,
     borderRadius: "0.5rem",
     "::-webkit-scrollbar": {
-      width: "1rem",
+      width: "0.75rem",
       backgroundColor: "var(--background)",
       borderTopRightRadius: "0.5rem",
       borderBottomRightRadius: "0.5rem",
     },
     "::-webkit-scrollbar-thumb": {
-      backgroundColor: "transparent",
-      border: "1px solid var(--foreground)",
+      backgroundColor: "var(--foreground-dim)",
       borderRadius: "0.5rem",
-      opacity: "0.2",
     },
   }),
-  option: (defaultStyles, { isSelected }) => {
-    return {
-      ...defaultStyles,
-      color: isSelected ? "var(--background)" : "var(--foreground)",
-      backgroundColor: isSelected ? "var(--foreground)" : "var(--background)",
-    };
-  },
+
+  option: (defaultStyles, { isSelected }) => ({
+    ...defaultStyles,
+    color: isSelected ? "var(--background)" : "var(--foreground)",
+    backgroundColor: isSelected ? "var(--foreground)" : "var(--background)",
+    ":hover": {
+      ...defaultStyles[":hover"],
+      backgroundColor: "var(--foreground-dim)",
+    },
+  }),
 };
 
 export default selectStyles;
